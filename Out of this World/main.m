37cbd436f0182f1fb8afcf1101b17f6459868f1b
Daniel Pratt <Daniel@Dans-MacBook-Air.local>
2014-07-30 12:08:13 -0700

    Initial Commit

---
+++ Out of this World/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Daniel Pratt on 7/30/14.
+//  Copyright (c) 2014 Daniel Pratt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
