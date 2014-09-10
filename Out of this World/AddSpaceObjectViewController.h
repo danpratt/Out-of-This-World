7d0825daa6ca1e6206cb8e4a73189f63f7c31713
Daniel Pratt <Daniel@Dans-MacBook-Air.local>
2014-09-10 09:33:26 -0700

    UI Changes

---
+++ Out of this World/AddSpaceObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by Daniel Pratt on 8/12/14.
+//  Copyright (c) 2014 Daniel Pratt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSpaceObjectViewController : UIViewController
+
+@end
